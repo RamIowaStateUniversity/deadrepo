8dc593f2001a900d86ae510705e0bb683d906c64
RamIowaStateUniversity <rramu@iastate.edu>
2016-05-21 00:15:48 -0500

    1

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void dead2(String[] a) {		
+			Scanner sc=new Scanner(System.in);
+			int y=90;
+			int b=1;
+			int j=0;
+			b=b+2;
+			while(j<2) {
+				System.out.println(compute(y));
+				j++;				
+			}
+	}
+}
